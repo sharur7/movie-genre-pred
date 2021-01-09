20668976fd746d320a60d3b3910c125a2d7e4d61
sharur7 <sharuraut7blog@gmail.com>
2021-01-09 12:47:59 +0530

    added demo

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict():
     	return render_template('after.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host="localhost", port=5000, debug=True)
