56825ef4f7352a0f1288cd9fd7dc72b5721f1e19
sharur7 <sharuraut7blog@gmail.com>
2021-01-09 20:43:59 +0530

    added port

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def predict():
 if __name__ == '__main__':
 	#app.run(host="localhost", port=3000, debug=True)
 	#log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
-	app.run(debug=True)
+	#app.run(debug=True)
+	port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port)
